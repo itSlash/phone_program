08f3a07c0c63e5fe5ae813d2572a0bd2c65f2641
itSlash <danil@mail.ru>
2022-10-07 22:33:26 +0300

    first commit

---
+++ app/src/main/java/com/example/mtsstartuphub/SplashScreen.java
@@ -0,0 +1,31 @@
+package com.example.mtsstartuphub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAY_LENGHT = 1500;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashScreen.this, MainActivity.class);
+                SplashScreen.this.startActivity(mainIntent);
+                SplashScreen.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGHT);
+    }
+
+    @Override
+    public void onBackPressed(){
+        super.onBackPressed();
+    }
+}
